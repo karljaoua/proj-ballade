f15402216d04a6f015d72a3c3d3781768ca3e2f6
karljaoua <jaoua@romandie.com>
2019-07-10 14:50:18 +0200

    Rajouter 3 balades pour une table non vide

---
+++ Ajouter3balades.sql
@@ -0,0 +1,24 @@
+INSERT INTO Balade (Titre, Type, Pseudo, Codepostal, Ville, Pays, Lieudepart, Lieuarrivee, Description, Etapes) VALUES (
+    
+  'Vers la Gruyère par les flancs du Gibloux', 'F', "Jeremy", 'CH-1705', 'Fribourg', 'Suisse', 'Fribourg-Centre', 'Gibloux',
+   
+  'Une longue randonnée reliant le chef-lieu du Canton de Fribourg au chef-lieu de la Gruyère. On retiendra, parmi les innombrables merveilles de cette randonnée, le lac de la Gruyère, véritable joyau, avec ses contours variés aux multiples anses, sans oublier les petits villages, les monts boisés.' ,
+   
+    'Fribourg - Ste-Apolline - Froideville (FR) - Ecuvillens - Posat - Farvagny-le-Grand - Vuisternens-en-Ogoz - Crête du Gibloux - Gibloux - Malessert - Petit Prary - La Grosse Moille - Neyruz - Pont sur la Sionge - Bulle'
+);
+
+INSERT INTO Balade (Titre, Type, Pseudo, Codepostal, Ville, Pays, Lieudepart, Lieuarrivee, Description, Etapes) VALUES (
+
+'Du district de la Glâne au district de la Veveyse', 'F', 'Marco', 'CH-1745', 'Romont', 'Suisse', 'Romont', 'Chatel-St-Denis',
+
+'Cette belle randonnée par vallons, champs et forêts, longeant rivières et ruisseaux vous conduit dans un décor naturel intact et plein de charme',
+
+'Romont - Villaraboud - Pra Mottes - Lieffrens - Les Mettraux - Le Crêt - Grattavache - Progens - Châtel-St-Denis' );
+
+INSERT INTO Balade (Titre, Type, Pseudo, Codepostal, Ville, Pays, Lieudepart, Lieuarrivee, Description, Etapes) VALUES (
+
+'Des ruines médiévales, un lac et une chapelle','S', 'Thierry', 'CH-1803', 'La Roche Gare', 'Suisse',
+'La Roche', 'Le Stald ',
+'Agréable promenade de détente dans une belle campagne où alternent champs, collines, haies et bosquets, belles fermes et ruines moyennageuses. ATTENTION ! Suite à quelques éboulements, ce parcours est sérieusement limité : Vers les Châteaux reste inaccessible.', 
+
+'La Roche - Vers les Châteaux - Pont-la-Ville - Chapelle de Thusy - Sur Momont - Le Stald' );
