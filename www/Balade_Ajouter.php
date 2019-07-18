a84d5cc34fd843277f22ef9464b2277dc9dcfd70
Will <william.rey@bluewin.ch>
2019-07-18 09:13:45 +0200

    ajouter balade
    will

--- www/Balade_Ajouter.php
+++ www/Balade_Ajouter.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <title>Bootstrap 4 Example</title>
+    <title>Balade_Ajouter.php</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,18 +14,27 @@
     
 </head>
 
-<body>
+<body class="fondB">
 
     <!------Section 6  Page du Right Food-->
     <section> <!-- hhh -->
-	
+        <div class="navb2">
+                <ul class="nav justify-content-center nb">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Balade_Ajouter.php"> Ajouter une randonnée</a>
+                    </li>
+                </ul>
+        </div>
         <div class="container-fluid">
 
             <div class="row">
 					
                 <div class="col-lg-12 categ-balades">
   
-                    <div class="mx-auto text-center title-action-balades mb-5">Ajouter une Nouvelle Balade</div>
+                    <h1 class="titrePrincipalA">Ajouter une nouvelle balade</h1>
 
                 </div> <!-- class="col-lg-12 -->
             </div>      <!-- Fin du 1er Row englobant Menu + Titre +Auteur -->  
@@ -35,11 +44,11 @@
             <div class="row">
 
                
-                <div class="col-lg-6 col-xs-12">
+                <div class="col-lg-7 col-xs-12">
 
                                             
 
-                    <form method="POST" action="insert-balade.php" >
+                    <form>
                         <div class="form-balade">                        
                              
                                 <div class="styledebalade" >
@@ -51,55 +60,52 @@
                             
 
                             <div class="form-group row">
+                                
+                                
 
-                                <label for="titre" class="col-lg-2 col-form-label lbl-balade">Titre de la balade: </label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="titre" class="form-control champsform" id="inputtitre" placeholder="Titre de la balade">
-                                </div>
-                            
                                 
                                 <label for="pseudo" class="col-lg-2 col-form-label lbl-balade">Pseudo Auteur: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="pseudo" class="form-control champsform" id="inputpseudo" placeholder="Pseudo de l'Auteur">
+                                    <input type="text" class="form-control champsform" id="inputpseudo" placeholder="Pseudo de l'Auteur">
                                 </div>
 
                                 <label for="codepostal" class="col-lg-2 col-form-label lbl-balade">Code postal: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="cp" class="form-control champsform" id="inputcp" placeholder="Code postal">
+                                    <input type="text" class="form-control champsform" id="inputcp" placeholder="Code postal">
                                 </div>
                                 
 
                                 <label for="ville" class="col-lg-2 col-form-label lbl-balade">Localité: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="ville" class="form-control champsform" id="inputville" placeholder="Localité">
+                                    <input type="text" class="form-control champsform" id="inputville" placeholder="Localité">
                                 </div>
                                 
 
                                 <label for="pays" class="col-lg-2 col-form-label lbl-balade">Pays: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="pays" class="form-control champsform" id="inputpays" placeholder="Suisse">
+                                    <input type="text" class="form-control champsform" id="inputpays" placeholder="Pays">
                                 </div>
                                 
                                 <label for="depart" class="col-lg-2 col-form-label lbl-balade">Départ: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="depart" class="form-control champsform" id="inputdepart" placeholder="Départ">
+                                    <input type="text" class="form-control champsform" id="inputdepart" placeholder="Départ">
                                 </div>
 
                                 <label for="arrivee" class="col-lg-2 col-form-label lbl-balade">Arrivée: </label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="arrivee" class="form-control champsform" id="inputpays" placeholder="Arrivée">
+                                    <input type="text" class="form-control champsform" id="inputpays" placeholder="Arrivée">
                                 </div>
 
                                 <label for="descrip" class="col-lg-2 col-form-label lbl-balade">Description: </label>
                                 
                                 <div class="col-sm-10">
-                                    <textarea class="form-control" rows="3" name="descrip" id="inputdescrip" placeholder="Description"></textarea>
+                                    <textarea class="form-control" rows="3" id="inputdescrip" placeholder="Description"></textarea>
                                 </div>
 
                                 <label for="itineraire" class="col-lg-2 col-form-label lbl-balade">Itinéraire: </label>
                                 <div class="col-sm-10">
                                     
-                                    <textarea class="form-control" rows="3" name="itineraire" id="inputitineraire" placeholder="Itinéraire"></textarea>
+                                    <textarea class="form-control" rows="3" id="inputitineraire" placeholder="Itinéraire"></textarea>
 
                                 </div>
 
@@ -109,21 +115,19 @@
 
                             
                         </div>
-
-                        <div class="btn-group-horizontal d-flex justify-content-around thebtn" role="group" aria-label="Basic example">
-               
-                            <button type="button" class="btn-lg btn-danger   modifier_btn " onClick="window.location.href='index.php';">Abandonner l'Ajout</button>
-                            <button type="submit" class="btn-lg btn-success  modifier_btn">Confirmer  l'Ajout</button>
-                            
-                            
-                        </div>
-
                     </form>
-                    
+
+
+                    <div class="btn-group-horizontal d-flex justify-content-around thebtn" role="group" aria-label="Basic example">
+               
+                            <button type="button" class="btn btn-outline-danger modifier_btn x1">Abandonner l'Ajout</button>
+                            <button type="button" class="btn btn-outline-success modifier_btn x1">Confirmer  l'Ajout</button>
+                    </div>
+                        
 
                 </div>
 
-                <div class="col-lg-6 col-xs-12 mb-5 d-block"> 
+                <div class="col-lg-5 col-xs-12 mb-5 d-block"> 
                     <!-- <div class="img-fluid float-right col-md-6 image_balade_box"> -->
                          
                     <div class="encadrage-image-balade">
