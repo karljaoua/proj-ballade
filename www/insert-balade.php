5c0cec2fcab0d83d1b299db71474105d983f6465
Will <william.rey@bluewin.ch>
2019-07-19 11:30:03 +0200

    suite css et karl

--- www/insert-balade.php
+++ www/insert-balade.php
@@ -12,9 +12,9 @@
 
     else {
         //echo "La connection à la Database " .$nombd ." a Réussi <br/>";
-        
+
     }
-    
+
     //echo $_FILES['imgfile'];
 
 
@@ -35,17 +35,17 @@
     $file_tmp =$_FILES['imgfile']['tmp_name'];
     $file_type=$_FILES['imgfile']['type'];
     $file_ext=strtolower(end(explode('.',$_FILES['imgfile']['name'])));
-    
+
     $extensions= array("jpeg","jpg","png");
-    
+
     if(in_array($file_ext,$extensions)=== false){
         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
     }
-    
+
     if($file_size > 2097152){
         $errors[]='File size must be excately 2 MB';
     }
-    
+
     if(empty($errors)==true){
 
         // Check if file already exists
@@ -56,65 +56,20 @@
     }else{
         print_r($errors);
     }
-    
-        
+
+
         $fichier_name = $_FILES['imgfile']['name'];
         $numbalad = $myconnection->insertBalade($titre, $pseudo, $type, $cp, $ville, $country, 
                                         $begin, $arrival, $descrip, $etap, $fichier_name);
-    
+
         //echo $numbalad;
-    
+
         $ph = "Location: Balade_Detail.php?id=" .$numbalad;
-    
-        
-    
+
+
+
         header($ph); 
-    
-    
+
+
         exit;
-
-    
-
-
-
-    
-        
-    
-    
-
-    
-
-
-        
-/*
-
-    $nombd='Promenades';
-    $user ='adminBalades';
-    $pwd  ='BAladesbalades';
-
-
-    $etap  ="Les Diablerets - Isenau station - Isenau - Col des Andérets - Chalet Vieux - ";
-    $etap .="Lac
-
-    $titre  = "Au-dessus du village des Diablerets";
-    $pseudo = "Adrien";
-    $type = "S";
-    $cp = "1863";
-    $ville = "Les Diablerets";
-    $country = "Suisse";
-    $begin   = "Les Diablerets";
-    $arrival = "La Corba";
-    $descrip = "Longue montée avec une vue sur les alpes vaudoises";
-
-    $etap  ="Les Diablerets - Isenau station - Isenau - Col des Andérets - Chalet Vieux - ";
-    $etap .="Lac Retaud - Col du Pillon - La Corba";
-
-    $foto = "https ://images0.schweizmobil.ch/image/Les_Diablerets__824_a.jpg";
-
-*/
-
-    
-
-
 ?>
-
